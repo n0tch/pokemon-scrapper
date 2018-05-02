582a725715a1dbc1997676f166d0b548d9e06937
n0tch <gusoliv3ira@gmail.com>
2018-05-02 00:28:18 -0300

    restructured folders

--- scrapperRun.py
+++
@@ -1,9 +0,0 @@
-from Scrapper import *
-
-if __name__ == '__main__':
-	url_base = 'https://pokemondb.net'
-	scrapper = Scrapper(url_base)
-	elements = get_elements()
-	element_list = elements_to_list(elements)
-	#dataFrame = list_to_dataFrame(element_list)
-	#print(dataFrame.head())
